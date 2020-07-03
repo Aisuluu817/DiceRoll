bddfb0eae8aa4cf585565cf49cf4c3ae6fda66cf
Aisuluu817 <a.arstanbaeva01@gmail.com>
2020-07-04 01:06:04 +0600

    landscape mode onSaveInstanceState()

--- app/src/main/java/com/example/diceroll/MainActivity.java
+++ app/src/main/java/com/example/diceroll/MainActivity.java
@@ -1,42 +1,82 @@
 package com.example.diceroll;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    Random random = new Random();
+    int [] arrOfDiceNum = {
+            R.drawable.dice1,
+            R.drawable.dice2,
+            R.drawable.ic_dice3,
+            R.drawable.dice4,
+            R.drawable.dice5,
+            R.drawable.dice6};
+    String [] comments = {
+            "One more time",
+            "Roll again!",
+            "Nice one!",
+            "Continue",
+            "Rollll",
+            "Good job!",
+            "Have a nice one",
+            "Good",
+            "Keep going",
+            "YAY!"};
+
+    int rollRight ;
+    int rollLeft ;
+    int commentRandom ;
+    Button roll;
+    ImageView imageViewRight;
+    ImageView imageViewLeft;
+    TextView textView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        roll = (Button) findViewById(R.id.roll);
+           imageViewLeft = findViewById(R.id.first);
+           imageViewRight = findViewById(R.id.second);
+           textView = findViewById(R.id.comments);
+           roll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                 rollLeft = random.nextInt(6);
+                rollRight = random.nextInt(6);
+                 commentRandom = random.nextInt(10);
 
-    int [] arrOfDiceNum = {R.drawable.dice1, R.drawable.dice2, R.drawable.ic_dice3,R.drawable.dice4, R.drawable.dice5,R.drawable.dice6};
-    Random random = new Random();
-    String [] comments = {"One more time", "Roll again!", "Nice one!", "Continue", "Rollll", "Good job!","Have a nice one", "Good", "Keep going", "YAY!"};
+                imageViewLeft.setImageResource(arrOfDiceNum[rollLeft]);
+                imageViewRight.setImageResource(arrOfDiceNum[rollRight]);
+                textView.setText(comments[commentRandom]);
+            }
+        });
+        if (savedInstanceState!= null) {
+            rollLeft = savedInstanceState.getInt("left");
+            rollRight = savedInstanceState.getInt("right");
+        }
 
-
-
-    public void roll(View view) {
-
-        ImageView imageViewRight = findViewById(R.id.first);
-        ImageView imageViewLeft = findViewById(R.id.second);
-        TextView textView = findViewById(R.id.comments);
-
-        int rollRight = random.nextInt(6);
-        int rollLeft = random.nextInt(6);
-        int commentRandom = random.nextInt(10);
-
-        imageViewRight.setImageResource(arrOfDiceNum[rollRight]);
         imageViewLeft.setImageResource(arrOfDiceNum[rollLeft]);
-        textView.setText(comments[commentRandom]);
+        imageViewRight.setImageResource(arrOfDiceNum[rollRight]);
 
+        textView.setText(comments[commentRandom]);
     }
-}
+
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("left",rollLeft);
+        outState.putInt("right",rollRight);
+    }
+    }
